0540e50783a4ce84f030143bb4d7f85b2ac7203b
ty <tyangpark@gmail.com>
2022-04-21 09:45:11 +0900

    h2 database exclude security

--- src/main/java/com/tyangpark/laboratory/WebSecurityConfig.java
+++ src/main/java/com/tyangpark/laboratory/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.tyangpark.laboratory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +14,13 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
