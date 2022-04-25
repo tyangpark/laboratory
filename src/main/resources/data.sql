79c3e11ebb953ddf79a79850824d1f1f2787450e
ty <tyangpark@gmail.com>
2022-04-25 09:19:30 +0900

    add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO customer (id, first_name, last_name)
+VALUES (1, 'TONY', 'PARK');
+INSERT INTO customer (id, first_name, last_name)
+VALUES (2, 'KEN', 'LEE');
+INSERT INTO customer (id, first_name, last_name)
+VALUES (3, 'EGG', 'KIM');
